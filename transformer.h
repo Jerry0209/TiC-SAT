c6de639eeb371f605fcb3e407ecd3d0d83a7184a
Jerry0209 <mr.htianrui@gmail.com>
2026-05-14 01:33:13 +0200

    add BERT base profiling executable

--- transformer.h
+++ transformer.h
@@ -65,18 +65,18 @@
 
 #else
 // BERT-mini
-#define D_Q 64
-#define D_SEQ 512
-#define D_MODEL 256
-#define NUM_HEAD 4
-#define D_FF 1024
-
-// Boss Menu (BERT-base) (Match codebooked GEMM)
 // #define D_Q 64
 // #define D_SEQ 512
-// #define D_MODEL 768
-// #define NUM_HEAD 12
-// #define D_FF 3072
+// #define D_MODEL 256
+// #define NUM_HEAD 4
+// #define D_FF 1024
+
+// Boss Menu (BERT-base) (Match codebooked GEMM)
+#define D_Q 64
+#define D_SEQ 512
+#define D_MODEL 768
+#define NUM_HEAD 12
+#define D_FF 3072
 
 #endif
 
